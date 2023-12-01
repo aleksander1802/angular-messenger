465fc131efc7cca48eb5447b47abdd421728b857
Alex2 <aleksander1802@gmail.com>
2023-12-01 15:26:18 +0300

    feat: added main-layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 const routes: Routes = [
-    
+    {
+        path: '',
+        component: MainLayoutComponent,
+        // children: [
+        //     {
+        //         path: '',
+        //         // redirectTo: 'login',
+        //         pathMatch: 'full',
+        //     },
+
+        //     // {
+        //     //     path: '**',
+        //     //     component: NotFoundComponent,
+        //     // },
+        // ],
+    },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CyberButtonComponent } from './components/cyber-button/cyber-button.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [CyberButtonComponent],
-    imports: [CommonModule],
+    declarations: [CyberButtonComponent, MainLayoutComponent],
+    imports: [CommonModule, RouterModule],
     exports: [CyberButtonComponent],
 })
 export class SharedModule {}
