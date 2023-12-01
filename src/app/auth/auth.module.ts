e3d90eb8b8ca2852ec502e878405e8022915aeb0
Alex2 <aleksander1802@gmail.com>
2023-12-01 15:41:54 +0300

    feat: added login page component to the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
-    declarations: [RegisterPageComponent],
+    declarations: [RegisterPageComponent, LoginPageComponent],
     imports: [CommonModule],
 })
 export class AuthModule {}
