3d43391b2a7f1c30b6bf22e2e88f4ddda6a3ef29
Alex2 <aleksander1802@gmail.com>
2023-12-03 14:20:16 +0300

    feat: added profile page component to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
     declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    ProfileComponent
   ],
     imports: [HttpClientModule],
 })
