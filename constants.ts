50eb8a36546c2c8b020ff5a5b761463f94e3f139
Alex2 <aleksander1802@gmail.com>
2023-12-05 14:10:44 +0300

    feat: updated constants list

--- constants.ts
+++ constants.ts
@@ -1,4 +1,24 @@
 export const APP_REGISTER = 'https://tasks.app.rs.school/angular/registration';
+
 export const APP_LOGIN = 'https://tasks.app.rs.school/angular/login';
+
 export const APP_PROFILE = 'https://tasks.app.rs.school/angular/profile';
+
 export const APP_LOGOUT = 'https://tasks.app.rs.school/angular/logout';
+
+export const APP_GROUPS_LIST =
+    'https://tasks.app.rs.school/angular/groups/list';
+
+export const APP_GROUPS_CREATE =
+    'https://tasks.app.rs.school/angular/groups/create';
+
+export const APP_GROUPS_DELETE =
+    'https://tasks.app.rs.school/angular/groups/delete?groupID={:groupID}';
+
+export const APP_USERS = 'https://tasks.app.rs.school/angular/users';
+
+export const APP_CONVERSATIONS_LIST =
+    'https://tasks.app.rs.school/angular/conversations/list';
+    
+export const APP_CONVERSATIONS_CREATE =
+    'https://tasks.app.rs.school/angular/conversations/create';
