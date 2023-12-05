e2f1614e4b7e88d806541f58402d93b2a2a50705
Alex2 <aleksander1802@gmail.com>
2023-12-05 14:52:38 +0300

    feat: added icons and background for dark and light themes

--- constants.ts
+++ constants.ts
@@ -19,6 +19,6 @@ export const APP_USERS = 'https://tasks.app.rs.school/angular/users';
 
 export const APP_CONVERSATIONS_LIST =
     'https://tasks.app.rs.school/angular/conversations/list';
-    
+
 export const APP_CONVERSATIONS_CREATE =
     'https://tasks.app.rs.school/angular/conversations/create';
