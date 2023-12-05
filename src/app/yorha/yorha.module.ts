dbfcda2630610e5a2169998bd9452049ab07da8a
Alex2 <aleksander1802@gmail.com>
2023-12-05 19:45:15 +0300

    feat: added grouplist component

--- src/app/yorha/yorha.module.ts
+++ src/app/yorha/yorha.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { YoRHaRoutingModule } from './yorha-routing.module';
+import { GrouplistComponent } from './components/grouplist/grouplist.component';
 
 @NgModule({
-    declarations: [HomePageComponent],
+    declarations: [HomePageComponent, GrouplistComponent],
     imports: [CommonModule, YoRHaRoutingModule],
 })
 export class YoRHaModule {}
